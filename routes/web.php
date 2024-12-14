6e51624f4060172ac7bf43b87ff91eb9e22981b2
Алексей <mail@ashatrov.ru>
2024-12-14 20:31:03 +0300

    Исправил CartManager.php

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SearchController;
 
-Route::view('/', 'index');
+// Route::view('/', 'index');
+Route::view('/', 'index2');
 Route::get('/search', [SearchController::class, 'search',]);
